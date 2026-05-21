a26bd6db89c7acdf61d7ee44b1d6e602c31555a4
LuFelix <luciano@tiweb.app.br>
2026-05-21 09:46:27 -0300

    fix(front): update environment urls to production domain (#40)
    Resolves CORS error on Google Login by pointing the frontend to the correct API domain.
    
    Closes #40

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mas.tiweb.app.br/api', 
+  apiUrl: 'https://smartcontact.tiweb.app.br/api', 
   googleClientId: '1018169699705-9k4vmo9lp3nkicj8hiv8alqleei7csje.apps.googleusercontent.com'
 
 };
