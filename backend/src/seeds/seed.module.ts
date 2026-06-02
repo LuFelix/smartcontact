666ad627e9b779895eb9050a18acb76be70b8405
LuFelix <luciano@tiweb.app.br>
2026-06-02 11:48:35 -0300

    fix(backend): inject MembershipRepository into SeedModule to resolve DI error on startup #163

--- backend/src/seeds/seed.module.ts
+++ backend/src/seeds/seed.module.ts
@@ -6,6 +6,7 @@ import { User } from 'src/users/entities/user.entity';
 import { Role } from 'src/roles/entities/role.entity';
 import { Tag } from 'src/tags/entities/tag.entity';
 import { Tenant } from 'src/tenants/entities/tenant.entity';
+import { Membership } from 'src/memberships/entities/membership.entity';
 import { SeedService } from './seed.service';
 import { UserSeedService } from './users/user-seed.service';
 import { ProfilesModule } from 'src/profiles/profiles.module';
@@ -14,7 +15,7 @@ import { UsersModule } from 'src/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forFeature([Role, User, Tag, Tenant]),
+    TypeOrmModule.forFeature([Role, User, Tag, Tenant, Membership]),
     ProfilesModule,
     UsersModule,
   ],
